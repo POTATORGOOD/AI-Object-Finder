cc8823a940ecc0e693d9177bcdf9784b5e5d5763
POTATORGOOD <101843193+POTATORGOOD@users.noreply.github.com>
2022-06-28 20:29:55 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 object = "";
-status = "";
+objects = [];
+status1 = "";
+synth = window.speechSynthesis;
 
 function setup() {
     canvas = createCanvas(480, 380);
@@ -16,15 +18,48 @@ function preload() {
 
 function draw() {
     image(video, 0, 0, 480, 380);
+
+    if (status1 != "") {
+        objectDetector.detect(video, gotResult);
+
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+
+            fill("red");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if (object == objects[i].label) {
+                video.stop();
+
+                speak_data = object;
+                var utterThis = new SpeechSynthesisUtterance(speak_data);
+                synth.speak(utterThis);
+
+                document.getElementById("object_status").innerHTML = "Object Mentioned Found";
+            }
+        }
+    }
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 
 function start() {
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status: Detecting Objects";
-    object = getElementById("object_input").value
+    object = document.getElementById("object_input").value;
 }
 
 function modelLoaded() {
     console.log("Model Loaded");
-    status = true;
+    status1 = true;
 }
